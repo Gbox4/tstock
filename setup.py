ca3c261292dd09d7cbaf556aba5d8d24be540150
Gabe Banks <gabe@gabebanks.net>
2022-01-16 11:05:17 -1000

    changed to use setup.py instead of cfg

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="tstock",
+    version="2.0.0",
+    author="Gabe Banks",
+    author_email="gabriel.t.banks@gmail.com",
+    description="A command line tool to view stock charts in the terminal.",
+    long_description="file: README.md",
+    long_description_content_type="text/markdown",
+    keywords="tstock stock ticker finance crypto",
+    url="https://github.com/Gbox4/tstock",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent"
+    ],
+    packages=["tstock"],
+    entry_points={"console_scripts": ["tstock=tstock:main"]},
+    python_requires=">=3.6")
